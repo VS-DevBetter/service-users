c2acbe7abafaef7631456661d2ba45d72a4a83be
Andre Fernandes <andrefernandes@Andres-Mac-mini.local>
2023-11-04 12:34:37 +0000

    add cors

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ connectDB();
 const app = express();
 const port = process.env.PORT || 8000;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
